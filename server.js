d804b7e8631824be1dc1e5e66202ba972d8a5274
shumihinigor <52691812+shumihinigor@users.noreply.github.com>
2021-04-25 23:01:03 +0300

    settings

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const history = require('connect-history-api-fallback');
 
-console.log("server start");
-
 const app = express();
 app.use(history());
 app.use(express.static('dist'));
@@ -11,4 +9,4 @@ app.get('/', (req, res) => {
   res.sendFile('dist/index.html');
 });
 
-app.listen(8080, () => console.log('server started'));
+// app.listen(8080, () => console.log('server started'));
