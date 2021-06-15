e1a7770e646128a812acba20a331c52967984916
shumihinigor <52691812+shumihinigor@users.noreply.github.com>
2021-06-15 22:38:08 +0300

    deploy catalog + pdf + fix error

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b0ee486673c7b2c0adac1311a6aa3918.js"
+  "/precache-manifest.8e3d733d8dde6a543cf114a2e9edb641.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "itline"});
